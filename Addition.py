e56b19cc375e4721837d343d2a18336518281295
Alex Kasujja <C00315323@setu.ie>
2025-09-15 21:47:36 +0000

    add

--- Addition.py
+++ Addition.py
@@ -1,13 +1,13 @@
 #Program asking the user for two numbers
 #adds them, and prints the result.
 #Ask the user for the first number
-num1 = input("Please enter first number: ")
+num1 = float(input("Please enter first number: "))
 
 #Ask the user for the second number 
-num2 = input(" Please enter the second number: ")
+num2 = float(input(" Please enter the second number: "))
 
 #Sum of the two numbers
-sum = num1 + num2 
+result = num1 + num2 
 
 #print the result 
-print(sum)
+print("The sum is: ", result)
