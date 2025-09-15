d7f9a8e85a2fb8dd1b9cf06ffc68e3e29b9f941d
Alex Kasujja <C00315323@setu.ie>
2025-09-15 22:02:17 +0000

    student

---
+++ student.py
@@ -0,0 +1,14 @@
+#Ask for the name of the student
+name = input("Please enter your name: ")
+
+#Ask for the age 
+age = input("Please enter your age: ")
+
+#Ask for the student number 
+studentNumber =input("Please enter your student number: ")
+
+#Print the result
+print("\n--- Student Information ---")
+print("Student Name:",name)
+print("Student age:",age)
+print("Student Number:",studentNumber)
